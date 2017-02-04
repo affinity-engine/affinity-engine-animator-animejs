b3353c10e6dc5b1295758d691ee61e07f81323f4
null null null <null@affinitygamescollective.org>
2017-02-04 10:31:31 -0800

    more fixes

--- app/affinity-engine/configs/animator-animejs.js
+++ app/affinity-engine/configs/animator-animejs.js
@@ -1,1 +1,1 @@
-export { default } from 'affinity-engine-animator-animejs/affinity-engine/configs/affinity-engine-animator-animejs';
+export { default } from 'affinity-engine-animator-animejs/affinity-engine/configs/animator-animejs';
